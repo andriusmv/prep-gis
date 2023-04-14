b977c20e7d5f3e05d3811877de11625fad544215
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2023-04-14 14:09:05 +0200

    added more

---
+++ pages/exam/ASPRS-GL-T/video.tsx
@@ -0,0 +1,61 @@
+import styles from '@/styles/Home.module.css'
+import supabase from '@/utils/supabase'
+import { GetStaticProps } from 'next'
+import Link from 'next/link';
+import ReactPlayer from 'react-player/lazy';
+
+interface ExamProps {
+    video: {id: string; title: string; embed: string; video_url: string}[];
+}
+
+export default function Video({ video }: ExamProps) {
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <aside className={styles.sidebar__sidebar}>
+          <div className={styles.main}>
+          <p className={styles.sidebar}>ASPRS Certified GIS/LIS Technologist</p>
+          <p className={styles.sidebar}><span>ASPRS-GL-T exam</span></p>
+          <br />
+          <h3 className={styles.sidebar}>🧛 Watch them all 👇📚</h3>
+          </div>
+        <ul className={styles.mainvideo}>
+          {video.map((video: {
+                id: string;
+                title: string; 
+                video_url: string;}) => (
+                <li key={video.id}>
+                <Link href={`/exam/ASPRS-GL-T/video#${video.id}`} className={styles.container}>{video.title}</Link>
+                </li>
+                ))}
+        </ul>
+        </aside>
+      <main className={styles.sidebar__main}>
+      {video.map((video: {
+                id: string;
+                title: string; 
+                embed: string;
+                video_url: string;}) => (
+        <div key={video.id} className={styles.main} id={video.id}>
+            <h2 className={styles.sidebar}>{video.title}</h2>
+          <ReactPlayer url={video.video_url} className={styles.videoyoutube} />
+      {/* <div className={styles.videocontainer}>
+          <iframe className={styles.video} src={`https://www.youtube.com/embed/${video.embed}`} width="500" height="281,25"/>
+          </div> */}
+          </div>
+                ))}
+      </main>
+      </div>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data: video } = await supabase.from("video").select().or('tag.cs.{ASPRS-GL-T}');
+  
+  return {
+      props: {
+        video,
+      },
+  };
+};
---
+++ pages/exam/ASPRS-MSUAS-S/index.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import supabase from '@/utils/supabase'
+import { GetStaticProps } from 'next'
+import Link from 'next/link';
+
+interface ExamProps {
+    video: {id: string; title: string; tag: string; video_url: string}[];
+}
+
+export default function Home({ video }: ExamProps) {
+  return (
+    <>
+      <Head>
+        <title>Certified Mapping Scientist, UAS exam</title>
+        <meta name="description" content="ASPRS-MSUAS-S exam" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+          <p>Currently showing curated videos for</p>
+          <h1>Certified Mapping Scientist, UAS</h1>
+          <h3>🔎 Simply click on the video of your choice</h3>
+        <div className={styles.grid}>
+          {video.map((video: {
+                id: string;
+                title: string; 
+                video_url: string;}) => (
+                <Link key={video.id} href={`/exam/ASPRS-MSUAS-S/video#${video.id}`} className={styles.card}>{video.title}</Link>
+            ))}
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data: video } = await supabase.from("video").select().or('tag.cs.{ASPRS-MSUAS-S}');
+  
+  return {
+      props: {
+        video,
+      },
+  };
+};
